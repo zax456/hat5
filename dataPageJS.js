50b3a53f6b1ad5b19bcf70220987645729a7993f
Ang Kian Hwee <kianhwee1994@hotmail.com>
2017-04-06 12:35:44 +0800

    no message

---
+++ dataPageJS.js
@@ -0,0 +1,8 @@
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages':['corechart']});
+
+$(document).ready(function() {
+      alert("Linking done");
+});
+
+var test = alert("hello");
