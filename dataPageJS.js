371efe56e737e9e952f12c864c55fe3028b72ebc
Ang Kian Hwee <kianhwee1994@hotmail.com>
2017-04-07 12:02:51 +0800

    no message

--- dataPageJS.js
+++ dataPageJS.js
@@ -1,6 +1,15 @@
-// Load the Visualization API and the corechart package.
-google.charts.load('current', {'packages':['corechart']});
-
 $(document).ready(function() {
-	
+	alert("hello");
+	// Load the Visualization API and the corechart package.
+	//google.charts.load('current', {'packages':['corechart']});
 });
+
+var typeViolence = {"Domestic", "Sexual"};
+var typeEmployment = {"first", "second"};
+var beforeViolenceDomestic = "What is your perception of family violence in Singapore?"; 
+var beforeViolenceSexual = "What is your perception of sexual violence in Singapore?"; 
+var beforeEmployment = "What is your perception of women employment status in Singapore?";
+
+function btnSubmit() {
+	alert("value");
+}
