0d01e439e9957947e8b169417f7659da0f7faa3e
Ang Kian Hwee <kianhwee1994@hotmail.com>
2017-04-07 12:04:10 +0800

    no message

--- dataPageJS.js
+++ dataPageJS.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	alert("hello");
+	alert("hello world");
 	// Load the Visualization API and the corechart package.
 	//google.charts.load('current', {'packages':['corechart']});
 });
