5fbe74535bb816665435462820ae4c405b8b690a
Ang Kian Hwee <kianhwee1994@hotmail.com>
2017-04-13 00:14:30 +0800

    Able to draw most of the charts now. Left collecting of users perceptions.

--- dataPageJS.js
+++ dataPageJS.js
@@ -10,9 +10,9 @@ var typeViolence = ["Physical", "Sexual"];
 var typeEmployment = ["Leadership", "Salary"];
 var typeEducation = ["Literacy", "Skills"];
 
-var violencePhysicalcountries = ["China", "India", "Japan", "Singapore"];
+var violencePhysicalcountries = ["India", "Japan", "Singapore"];
 var employmentLeadershipcountries = ["China", "Japan", "Singapore"]; 
-var educationLiteracycountries = ["India", "Singapore"];
+var educationLiteracycountries = ["Singapore"];
 
 var beforeViolence = ["Q1: Who do you think has a higher chance of being violated?"];
 var beforeEmployment = ["Q1: Who would you prefer to be your supervisor?", "Q2: Do you think it matters what gender your superior is?"];
@@ -333,38 +333,165 @@ function categoryTypebtnSubmit() {
 
 var createdChart = 0;
 function drawChart(category) {
+	console.log(category);
+
 	if (createdChart == 1) {
 		removeDropChart();
 		createdChart = 0;
 	}
+
   switch(category) {
     case "violence Physical Singapore":
     	var numOfGraphs = 3;
     	var graphsURL = ["https://docs.google.com/spreadsheets/d/1uhh7d5oAUXaQMX7SeqrCC39DCHkLwDdh_s_HQ34ny7o/gviz/tq?range=A:B", "https://docs.google.com/spreadsheets/d/1UtWiiUXKtLmXRcJiXISE6qlrDTYyeKAvMammsVjZd0A/gviz/tq?range=A1:D6", "https://docs.google.com/spreadsheets/d/1B1iGs7YlxE7unDMc3xD-r5iVvEV6J8dUW-zX0NrI1ag/gviz/tq?range=A:B"];
-      //draw data from google spreadsheet to create graph 
-      for(var i = 0; i<numOfGraphs; i++) {
-      	var q = new google.visualization.Query(graphsURL[i]);
-      	q.send(handleQueryResponse2);
-      }
-      createdChart = 1;
-      break;
+      	//draw data from google spreadsheet to create graph 
+      	for(var i = 0; i<numOfGraphs; i++) {
+      		var q = new google.visualization.Query(graphsURL[i]);
+      		q.send(handleQueryResponse2);
+      	}
+
+      	createdChart = 1;
+      	break;
 
     case "violence Physical Japan": 
     	var graphsURL = "https://docs.google.com/spreadsheets/d/1Kg-otTTr5ZbSolu5-CnubXnb1qol5-jicfFhF5MfI3k/gviz/tq?range=A:B";
     	var q = new google.visualization.Query(graphsURL);
     	q.send(handleQueryResponse2);
 
-    case "violence Physical ": 
     	createdChart = 1;
-      break;
+    	break;
 
-    case "leadership":
-      createdChart = 1;
-      break;
+    case "violence Physical India": 
+    	var graphsURL = "https://docs.google.com/spreadsheets/d/1Tq224nGPkvNZD2h8UQDCpwKtmhveQ0hCoDaiSSoR_vQ/gviz/tq?range=A:B";
+    	var q = new google.visualization.Query(graphsURL);
+    	q.send(handleQueryResponse2);
+
+    	createdChart = 1;
+      	break;
+
+    case "violence Sexual Singapore":
+    	var graphsURL = ["https://docs.google.com/spreadsheets/d/1LEAF596FA3vK37phBlk9I1lv7wtN3P_-LxvMSyy5HIo/gviz/tq?range=A1:C7",
+    					"https://docs.google.com/spreadsheets/d/1STJ1u9EB_PpS77FrVjl-dLKGBpwFRxrwcgcF8ZBYbjI/gviz/tq?range=A1:D5", 
+    					"https://docs.google.com/spreadsheets/d/19e4d6AM00VeAzDArLNmrxr-T9qHnUmiDMQj4JVcN7QI/gviz/tq?range=A1:B3"];
+    	var numOfGraphs = 3;
+    	for(var i = 0; i<numOfGraphs; i++) {
+      		var q = new google.visualization.Query(graphsURL[i]);
+      		q.send(handleQueryResponse2);
+      	}
+
+    	createdChart = 1;
+      	break;
+
+    case "violence Sexual Japan": 
+    	var q = new google.visualization.Query("https://docs.google.com/spreadsheets/d/1d4feYuCgHlA2dH_vEu8wxhhQT3KdGNacg-9sd9Vd3fI/gviz/tq?range=A1:B4");
+    	q.send(handleQueryResponse2);
+
+    	createdChart = 1;
+    	break;
+
+    case "violence Sexual India":
+    	var graphsURL = ["https://docs.google.com/spreadsheets/d/1da2Gcg7omkfDnWcgTrBGBytxDd1MNphYsU5w_4nlx9g/gviz/tq?range=A1:C6", "https://docs.google.com/spreadsheets/d/165cetp2dy5VyTTmduRQ5NsfyMU7Fyq3LT0x_reLTcf0/gviz/tq?range=A1:C4"];
+    	var numOfGraphs = 2;
+    	for(var i = 0; i<numOfGraphs; i++) {
+      		var q = new google.visualization.Query(graphsURL[i]);
+      		q.send(handleQueryResponse2);
+      	}
+
+    	createdChart = 1;
+    	break;
+
+    case "employment Leadership Singapore":
+    	var q = new google.visualization.Query("https://docs.google.com/spreadsheets/d/1w0dXHg9H4kk-YacosxFcijAaV5h6RleDEkTwSD7THQg/gviz/tq?range=A1:B4");
+    	q.send(handleQueryResponse2);
+
+    	createdChart = 1;
+    	break;
+
+    case "employment Leadership China": 
+    	var q = new google.visualization.Query("https://docs.google.com/spreadsheets/d/1MafD6FxJbQ9WjoLzqGfawn9RvJucS8YXvh-bfqW2mQQ/gviz/tq?range=A1:B4");
+    	q.send(handleQueryResponse2);
+
+    	createdChart = 1;
+    	break;
+
+    case "employment Leadership Japan": 
+    	var q = new google.visualization.Query("https://docs.google.com/spreadsheets/d/141udVjAT4hOSfBS3q61TX7BBxvHEj2j6vXVyMKGwWig/gviz/tq?range=A1:B4");
+    	q.send(handleQueryResponse2);
+
+    	createdChart = 1;
+    	break; 
+
+    case "employment Salary Singapore": 
+    	var q = new google.visualization.Query("https://docs.google.com/spreadsheets/d/1oQram8qoTdiVjEwCct2vqL68uKQjBFO_TDDfe6MOqeo/gviz/tq?range=A1:B3");
+    	q.send(handleQueryResponse2);
+
+    	createdChart = 1;
+    	break; 
+
+    case "employment Salary Japan": 
+    	var q = new google.visualization.Query("https://docs.google.com/spreadsheets/d/1aKhIf-a6gj-bFVKUSW62mEKtbGSCTcYQTfzFwAB6OIs/gviz/tq?range=A1:B16");
+    	q.send(handleQueryResponse3);
+    	
+    	createdChart = 1;
+    	break;
+
+    case "employment Salary China": 
+    	var graphsURL = ["https://docs.google.com/spreadsheets/d/1qQBwa-9MnP1NUpEzWh6oSgdOGU4D148IQGL_2BwBP4o/gviz/tq?range=A1:B3", "https://docs.google.com/spreadsheets/d/1H80BOVkDL_uBiO2v4Zrrgok7VqI0RO26UMBjN7P_j14/gviz/tq?range=A1:C3"];
+    	var numOfGraphs = 2;
+    	for(var i = 0; i<numOfGraphs; i++) {
+      		var q = new google.visualization.Query(graphsURL[i]);
+      		q.send(handleQueryResponse2);
+      	}
+
+    	createdChart = 1;
+    	break;
+
+    case "education Literacy Singapore":
+    	var graphsURL = ["https://docs.google.com/spreadsheets/d/1pFCxzFl6TUH2HvFJk4Iu5rDEHoKXmzKBqV2SvDByXAY/gviz/tq?range=A1:B5", 
+    						"https://docs.google.com/spreadsheets/d/1cNzeilEHTjboMDJIcXNZBk2ZCNUEuAMdmpxA34Gw-1I/gviz/tq?range=A1:G7", 
+    						"https://docs.google.com/spreadsheets/d/1Yw_URqMiu2Xepso1SNmhFEXOmBPiq1skB-km1yn27r8/gviz/tq?range=A1:B3"];
+    	var numOfGraphs = graphsURL.length;
+    	for(var i = 0; i < numOfGraphs; i++) {
+    		var q = new google.visualization.Query(graphsURL[i]);
+    		q.send(handleQueryResponse2);
+    	}
+
+    	createdChart = 1;
+    	break;
+
+    case "education Literacy India":
+    	var graphsURL = ["https://docs.google.com/spreadsheets/d/1J9XPeQnHwd5aGM4O7UJqweR-UpaCx6BlUUSiGlFaFaE/gviz/tq?range=A1:C3", "https://docs.google.com/spreadsheets/d/1d25rCYFoUrZJGTHRf4-PKnDYfumM0xRrLqIRDRM9Xbk/gviz/tq?range=A1:D4"];
+    	var numOfGraphs = graphsURL.length;
+    	for(var i = 0; i < numOfGraphs; i++) {
+    		var q = new google.visualization.Query(graphsURL[i]);
+    		q.send(handleQueryResponse2);
+    	}
+
+    	createdChart = 1;
+    	break;
+
+    case "education Skills Singapore":
+    	var graphsURL = ["https://docs.google.com/spreadsheets/d/1HqqCaHsElNTOrM9jtYmqRBTU-6AOU7LhCeMZWw3ZQws/gviz/tq?range=A1:B3", 
+    					"https://docs.google.com/spreadsheets/d/18UvI6x2EtnKVN9MAnl9Tb86alp1e51_sYSAco21EtIc/gviz/tq?range=A1:C11"];
+    	var numOfGraphs = graphsURL.length;
+    	for(var i = 0; i < numOfGraphs; i++) {
+    		var q = new google.visualization.Query(graphsURL[i]);
+    		q.send(handleQueryResponse2);
+    	}
+
+    	createdChart = 1;
+    	break;
+
+    case "education Skills India": 
+    	var q = new google.visualization.Query("https://docs.google.com/spreadsheets/d/1DXEk4OZ78x-HjA7lQsiQ0l2Jjpa5Q8zFrL5r9caU7zU/gviz/tq?range=B1:G8");
+    	q.send(handleQueryResponse2);
+
+    	createdChart = 1;
+    	break;
   }
 }
 
-function handleQueryResponse2(response) {
+function handleQueryResponse2(response) { //for bar charts
 	//for debugging purpose
 	if (response.isError()) {
         alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
@@ -384,41 +511,17 @@ function handleQueryResponse2(response) {
     areaToAppendChart.appendChild(helper);
   } 
 
-// OLD GRAPH functions (ignore the below 2 functions)
-function readGoogleSheet(url) {
-  $.get({
-        url: url,
-        success: function(response) {
-          console.log(response.feed.entry);
-          var arr = response.feed.entry; 
-          var titleArr = new Array(); //to store the different age groups 
-          var contentArr = new Array(); //to store the percentage for each group
-          for(var i=0; i<arr.length; i++) { 
-            var eachRowTitle = arr[i].title.$t; //get the age group 
-            var eachRowContent = arr[i].content.$t; //get the % 
-            eachRowContent = eachRowContent.split(":");
+ function handleQueryResponse3(response) { //for line charts
+ 	var data = response.getDataTable();
+    var areaToAppendChart = document.getElementById("chartDiv");
+    var helper = document.createElement('div');
+    helper.setAttribute('style', 'width:100%; height:100%;');
 
-            titleArr.push(eachRowTitle); //add age group to titleArr
-            contentArr.push(eachRowContent[1].trim()); //add % to contentArr
-          }
 
-          //format Arrays to dataTable 
-          var dataTable = google.visualization.arrayToDataTable([
-            ['Abuse', 'Percentage'], 
-            [titleArr[0], Number(contentArr[0])],
-            [titleArr[1], Number(contentArr[1])]
-            ]);
-          handleQueryResponse(dataTable);
-        }
-      });
-}
+    var chart = new google.visualization.LineChart(helper);
+	// Set chart options
+	var options = {title: 'Gender wage gap vs. Year', width: 860, height: '400',backgroundColor: 'lightblue'};
+	chart.draw(data, options);
+	areaToAppendChart.appendChild(helper);
 
-function handleQueryResponse(dataTable) {
-    var data = dataTable;
-    var chart = new google.visualization.PieChart(document.getElementById('chartArea'));
-    // Set chart options
-      var options = {'title':'Percentage of women abuse in Singapore',
-                     'backgroundColor': 'lightblue'
-                     };
-    chart.draw(data, options);
-  }
+ }
